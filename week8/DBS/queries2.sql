8477cee11854b72354c9286f064e0ac837035293
Nimor111 <georgi.bojinov@hotmail.com>
2016-12-11 18:28:55 +0200

    Adding some db exercises bored

--- week8/DBS/queries2.sql
+++ week8/DBS/queries2.sql
@@ -1,33 +1,15 @@
-SELECT movie.title, movie.year, movie.studioname, studio.name
-FROM movie, studio
-WHERE movie.length > 120 AND movie.studioname = studio.name;
+-- QUERY 2--
+SELECT products.productname , products.unitprice
+FROM products
+ORDER BY products.unitprice DESC
+LIMIT 10;
 
-SELECT movie.studioname, starsin.starname
-FROM starsin, movie
-WHERE starsin.movietitle = movie.title
-ORDER BY movie.studioname;
+-- QUERY 3 --
+SELECT products.categoryid, COUNT (products.categoryid)
+FROM products
+GROUP BY products.categoryid;
 
-SELECT DISTINCT movieexec.name
-FROM movieexec, movie, starsin
-WHERE movie.producer = movieexec.CERT AND movie.title = starsin.movietitle
-AND starsin.starname = "Harrison Ford";
-
-SELECT moviestar.name
-FROM moviestar, movie, starsin
-WHERE moviestar.gender = "F"
-AND movie.title = starsin.movietitle
-AND moviestar.name = starsin.starname
-AND movie.studioname = "MGM";
-
-SELECT movieexec.name, movie.title
-FROM movieexec, movie
-WHERE movieexec.CERT = movie.producer
-AND movieexec.name = (SELECT movieexec.name 
-					   FROM movieexec, movie
-					   WHERE movie.producer = movieexec.CERT
-					   AND movie.title = "Star Wars");
-
-SELECT moviestar.name
-FROM moviestar
-WHERE moviestar.name NOT IN (SELECT starsin.starname
-							 FROM starsin);
+--QUERY 4--
+SELECT DISTINCT shippers.companyname
+FROM orders, shippers
+WHERE orders.shippeddate > 9/26/1996;
