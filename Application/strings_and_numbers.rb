867b21df9ba8aab65d834e4bd00c3b9a7d0e604c
Nimor111 <georgi.bojinov@hotmail.com>
2016-12-14 01:13:47 +0200

    Fix ruby code with rubocop ( san not finished )

--- Application/strings_and_numbers.rb
+++ Application/strings_and_numbers.rb
@@ -1,37 +1,35 @@
-def decode(string) #string is a string aight
+def decode(string) # string is a string aight
+  decode_hash = Hash.new # hash to hold characters and times they are
+  # repeated
 
-	decode_hash = Hash.new # hash to hold characters and times they are
-	# repeated
-
-	counter = 1
-	0.upto(string.length) do |i|
-		until(string[i] != decode_hash.key(decode_hash[string[i]]))
-			if ( i > string.length ) then break else
-				i += 1
-			end
-		end
-		(i+1).upto(string.length) do |j|
-			if ( string[j] == string[i] ) then counter += 1 end
-		end
-		decode_hash[string[i]] = counter
+  counter = 1
+  0.upto(string.length) do |i|
+   until string[i] != decode_hash.key(decode_hash[string[i]])
+     if i > string.length then break else i += 1
+     end
+   end
+     (i + 1).upto(string.length) do |j|
+			if string[j] == string[i] then counter += 1 end
+		 end
+    decode_hash[string[i]] = counter
 		counter = 1
 	end
 	decode_hash.delete(nil) # I really don't get this part
 
-	decode_hash = decode_hash.sort_by {|key,value| value}.reverse.slice(0,10).to_h # take first 10 most repeating characters to decode
+	decode_hash = decode_hash.sort_by { |_, value| value }
+                          .reverse.slice(0, 10).to_h
+  # take first 10 most repeating characters to decode
 
-	string.split('').each { |i| if ( decode_hash.key?(string[i]) ) then string[i] = decode_hash.keys.reverse.
-	index(decode_hash.key(decode_hash[string[i]])).to_s end}
+string.split('').each { |i| if ( decode_hash.key?(string[i]) ) then string[i] = decode_hash.keys.reverse.index(decode_hash.key(decode_hash[string[i]])).to_s end }
 
-	return string
-
+    string
 end
 
 def calculate_sum_of_numbers(string)
-	result = 0
-	string = string.scan(/\d+/) # isolate numbers in an array, ignoring all other characters
-	string.each { |i| result += i.to_i}
-	return result
+  result = 0
+  string = string.scan(/\d+/) # isolate numbers in an array, ignoring all other characters
+  string.each { |i| result += i.to_i }
+  result
 end
 
 puts calculate_sum_of_numbers(decode('bbcccddddeeeeeffffffggggggghhhhhhhhiiiiiiiiijjjjjjjjjja'))
