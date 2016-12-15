fb9ed9a93d7dcdd717679946afe95a4d70c79ab4
Nimor111 <georgi.bojinov@hotmail.com>
2016-12-16 00:46:30 +0200

    3 queries left to do, API almost done, TODO prettytable

--- week9/Hospital/Hospital-Task/hospital_gui.py
+++ week9/Hospital/Hospital-Task/hospital_gui.py
@@ -20,24 +20,22 @@ class HospitalAPI:
     def list_all_patients(self):
         patients = """
         SELECT *
-        FROM PATIENT;
+        FROM PATIENT
         """
         result = self.cursor.execute(patients)
         i = 0
         for row in result:
-            i += 1
-            print('{}'.format(i), row['firstname'], row['lastname'])
+            print(row['id'], row['firstname'], row['lastname'])
 
     def list_all_doctors(self):
         doctors = """
         SELECT *
-        FROM DOCTOR;
+        FROM DOCTOR
         """
         result = self.cursor.execute(doctors)
         i = 0
         for doctor in result:
-            i += 1
-            print('{}'.format(i), doctor['firstname'], doctor['lastname'])
+            print(doctor['id'], doctor['firstname'], doctor['lastname'])
 
     def add_patient(self, firstname, lastname, age,
                     gender, doctor):
@@ -68,32 +66,61 @@ class HospitalAPI:
         self.cursor.execute(query, (room, startdate, enddate, injury, patient))
         self.__commit()
 
-    def __update_patient(self, attr, patient, value):
+    def __update_patient(self, attr, patient_id, value):
         query = """
         UPDATE PATIENT
         SET {0} = ?
-        WHERE patient.firstname = ? AND patient.lastname = ?
+        WHERE patient.id = ?
         """.format(attr)
 
-        self.cursor.execute(query, (value,
-                                    patient.split(' ')[0],
-                                    patient.split(' ')[1]))
+        self.cursor.execute(query, (value, patient_id))
         self.__commit()
 
-    def __update_doctor(self, attr, doctor, value):
+    def __update_doctor(self, attr, doctor_id, value):
         query = """
         UPDATE DOCTOR
         SET {0} = ?
-        WHERE doctor.firstname = ? AND doctor.lastname = ?
+        WHERE doctor.id = ?
         """.format(attr)
 
-        self.cursor.execute(query, (value,
-                                    doctor.split(' ')[0],
-                                    doctor.split(' ')[1]))
+        self.cursor.execute(query, (value, doctor_id))
         self.__commit()
 
+    def __update_hospital_stay(self, attr, patient_id, value):
+        query = """
+        UPDATE HOSPITAL_STAY
+        SET {0} = ?
+        WHERE hospital_stay.patient IN (SELECT
+                                        FROM patient
+                                        WHERE patient.id = ?
+        """.format(attr)
+
+        self.cursor.execute(query, (value, patient_id))
+        self.__commit()
+
+    def update_hospital_information(self):
+        print("Update stay of patient? (id)")
+        update = input()
+        print("Attribute to change? (room, startdate, enddate, \
+                                     injury, patient? ")
+        change = input()
+        print("New information: ")
+        new = input()
+        if change == "room":
+            self.__update_hospital_stay("room", update, new)
+        elif change == "startdate":
+            self.__update_hospital_stay("startdate", update, new)
+        elif change == "enddate":
+            self.__update_hospital_stay("enddate", update, new)
+        elif change == "injury":
+            self.__update_hospital_stay("injury", update, new)
+        elif change == "patient":
+            self.__update_patient("patient", update, new)
+        else:
+            print("Invalid information!")
+
     def update_doctor_information(self):
-        print("Update doctor? (first name, last name?)")
+        print("Update doctor? (id)")
         change = input()
         print("Doctor current first name and last name: ")
         update = input()
@@ -103,11 +130,13 @@ class HospitalAPI:
             self.__update_doctor("firstname", update, new)
         elif change == "last name":
             self.__update_doctor("lastname", update, new)
+        else:
+            print("Invalid information!")
 
     def update_patient_information(self):
         print("Update patient? (first name, last name, age, gender, doctor?)")
         change = input()
-        print("Patient current first name and last name: ")
+        print("Patient id?")
         update = input()
         print("New information: ")
         new = input()
@@ -124,29 +153,84 @@ class HospitalAPI:
         else:
             print("Invalid information!")
 
-    def __delete_patient(self, fname, lname):
+    def __delete_patient(self, patient_id):
         query = """
-        DELETE FROM PATIENT
-        WHERE patient.firstname = ? AND patient.lastname = ?
+        DELETE
+        FROM PATIENT
+        WHERE patient.id = ?;
         """
-        self.cursor.execute(query, (fname, lname))
+        self.cursor.execute(query, patient_id)
+        self.__commit()
+
+    def __delete_doctor(self, doctor_id):
+        query = """
+        DELETE
+        FROM DOCTOR
+        WHERE doctor.id = ?
+        """
+        self.cursor.execute(query, (patient_id))
+        self.__commit()
+
+    def __delete_hospital_stay(self, patient_id):
+        query = """
+        DELETE
+        FROM hospital_stay
+        WHERE hospital_stay.patient IN ( SELECT patient.id
+                                         FROM patient
+                                         WHERE patient.id = ?
+        """
+        self.cursor.execute(query, (patient_id))
         self.__commit()
 
     def delete_patient(self):
-        print("Delete patient? (firstname lastname)")
+        print("Delete patient? (id)")
         to_del = input()
-        self.__delete_patient(to_del.split(' ')[0], to_del.split(' ')[1])
+        self.__delete_patient(to_del)
+
+    def delete_doctor(self):
+        print("Delete doctor? (id)")
+        to_del = input()
+        self.__delete_doctor(to_del)
+
+    def delete_hospital_stay(self):
+        print("Delete stay? (patient id)")
+        to_del = input()
+        self.__delete_hospital_stay(to_del)
+
+    def __list_all_patients_doctor(self, fname, lname):
+        query = """
+        SELECT
+        GROUP_CONCAT(patient.firstname || " " || patient.lastname) AS patients
+        FROM doctor
+        LEFT JOIN patient
+        ON doctor.id = patient.doctor
+        WHERE doctor.firstname = ? AND doctor.lastname = ?
+        GROUP BY doctor.id
+        """
+        result = self.cursor.execute(query, (fname, lname))
+
+        for row in result:
+            print(row['patients'])
+            # ('GROUP_CONCAT(patient.firstname, patient.lastname)'))
+
+    def all_doctor_patients(self):
+        print("Which doctor's patients would you like to see?")
+        print("Input in format: firstname lastname")
+        doctor = input()
+        self.__list_all_patients_doctor(doctor.split(' ')[0],
+                                        doctor.split(' ')[1])
 
 
 def main():
     hospital = HospitalAPI('hospital.db')
-    hospital.add_patient("Pesho", "Peshov", 12, 'M', 5)
-    hospital.add_doctor("Courtney", "Cake")
+    # hospital.add_patient("Pesho", "Peshov", 12, 'M', 1)
+    # hospital.add_doctor("Courtney", "Cake")
     hospital.delete_patient()
     # hospital.update_doctor_information()
-    # hospital.update_patient_information()
+    hospital.update_patient_information()
     hospital.list_all_patients()
-    hospital.list_all_doctors()
+    # hospital.list_all_doctors()
+    # hospital.all_doctor_patients()
 
 
 if __name__ == '__main__':
