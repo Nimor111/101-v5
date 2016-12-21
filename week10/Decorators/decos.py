1ef736f686f667b801e235f37de748d04c0fbd90
Nimor111 <georgi.bojinov@hotmail.com>
2016-12-21 16:35:23 +0200

    Add changes to decos

--- week10/Decorators/decos.py
+++ week10/Decorators/decos.py
@@ -1,14 +1,17 @@
-def accepts(*args):
-    type_lst = args
+from functools import wraps
+
+
+def accepts(*types, **kwargs):
 
     def accepter(func):
-        def check_types(*args):
+        @wraps(func)
+        def decorator(*args, **kwargs):
             for arg in range(len(args)):
-                if type(args[arg]) != type_lst[arg]:
+                if type(args[arg]) != types[arg]:
                     raise TypeError("Argument {} of say_hello is not {}"
-                                    .format(arg + 1, type_lst[arg]))
-            return func(*args)
-        return check_types
+                                    .format(arg + 1, types[arg]))
+            return func(*args, **kwargs)
+        return decorator
     return accepter
 
 
@@ -17,27 +20,32 @@ def say_hello(name, money):
     return "Hello, I am {}".format(name)
 
 
+def encode(string, key):
+    string = list(string)
+    for sym in range(len(string)):
+        if string[sym] != ' ':
+            string[sym] = chr(ord(string[sym]) + key % 26)
+    return ''.join(string)
+
+
 def encrypt(key):
 
     def accepter(func):
-        def encrypter():
-            res = func()
-            res = list(res)
-            for sym in range(len(res)):
-                if res[sym] != ' ':
-                    res[sym] = chr(ord(res[sym]) + key % 26)
-            return ''.join(res)
-        return encrypter
+        @wraps(func)
+        def decorator(*args, **kwargs):
+            res = func(*args, **kwargs)
+            return encode(res, key)
+        return decorator
     return accepter
 
 
 @encrypt(2)
-def get_low():
-    return "Get get get low"
+def get_low(a):
+    return "Get get get low" * a
 
 
 def main():
-    pass
+    print(get_low(1))
 
 
 if __name__ == '__main__':
