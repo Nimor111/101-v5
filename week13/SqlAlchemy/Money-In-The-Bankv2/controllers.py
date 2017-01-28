328fa2c554aee5c0e7a0c17b28f9873f7b3f67e7
Nimor111 <georgi.bojinov@hotmail.com>
2017-01-29 00:27:10 +0200

    revert back

--- week13/SqlAlchemy/Money-In-The-Bankv2/controllers.py
+++ week13/SqlAlchemy/Money-In-The-Bankv2/controllers.py
@@ -3,9 +3,9 @@ from sqlalchemy import create_engine
 
 from models import Client
 
-from settings import DB_NAME
+import settings
 
-engine = create_engine(DB_NAME)
+engine = create_engine(settings.DB_NAME)
 
 Session = sessionmaker(bind=engine)
 
