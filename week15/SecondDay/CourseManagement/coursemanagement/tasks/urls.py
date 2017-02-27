ce767bd59dd379ed015b9d5ae97e0210c0fecf66
Nimor111 <georgi.bojinov@hotmail.com>
2017-02-27 21:21:08 +0200

    Task logic finished

--- week15/SecondDay/CourseManagement/coursemanagement/tasks/urls.py
+++ week15/SecondDay/CourseManagement/coursemanagement/tasks/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     url(r'^$', views.TaskListView.as_view(), name='index'),
     url(r'^add$', views.TaskCreateView.as_view(), name='add_task'),
     url(r'^(?P<pk>[0-9]+)', views.TaskDetailView.as_view(), name='t_detail'),
+    url(r'^edit/(?P<pk>[0-9]+)', views.TaskUpdateView.as_view(),
+        name='task_edit'),
+    url(r'^course/(?P<pk>[0-9]+)', views.TaskCourseListView.as_view(),
+        name='course_tasks'),
 ]
--- week15/SecondDay/CourseManagement/coursemanagement/tasks/views.py
+++ week15/SecondDay/CourseManagement/coursemanagement/tasks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.views import generic
 
 from .models import Task
+from courses.models import Course
 
 from .forms import TaskForm
 
@@ -31,3 +32,31 @@ class TaskDetailView(generic.DetailView):
 
     def get_queryset(self):
         return Task.objects.filter(pk=self.kwargs['pk'])
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    template_name = 'tasks/edit_task.html'
+    form_class = TaskForm
+
+    def get_success_url(self):
+        return '/task/{}'.format(self.kwargs['pk'])
+
+
+class TaskCourseListView(generic.ListView):
+    template_name = 'tasks/course_tasks.html'
+    context_object_name = 'course_tasks'
+
+    def get_queryset(self):
+        """
+        Return tasks for a course.
+        """
+        return Task.objects.filter(course=Course.
+                                   objects.get(pk=self.kwargs['pk']))
+        # import ipdb; ipdb.set_trace()
+
+    def get_context_data(self, **kwargs):
+        context = super(TaskCourseListView, self).get_context_data(**kwargs)
+        course = Course.objects.get(pk=self.kwargs['pk'])
+        context['course'] = course
+        return context
